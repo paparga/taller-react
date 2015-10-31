cef043ce9163cb6c29d0c57a966e02567f4249c5
Pablo Parga <pargag@gmail.com>
2015-10-31 12:47:05 -0300

    test.js

---
+++ spec/index.js
@@ -0,0 +1,7 @@
+const {expect} = require('chai')
+const sinon = require('sinon')
+const Nuclear = require('nuclear-js')
+const flux = require('../app/flux')
+const getters = require('../app/getters')
+const actions = require('../app/actions')
+const stores = require('../app/stores')
